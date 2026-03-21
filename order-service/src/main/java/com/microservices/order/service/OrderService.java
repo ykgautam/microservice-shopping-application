478ae41b3ab8d0f363129fa6c42f7844870cc627
Yashkumar Gautam <yk.gautam97@gmail.com>
2026-03-21 17:37:52 +0530

    #order-service used FeignClient for synchronized communication between order and inventory services

--- order-service/src/main/java/com/microservices/order/service/OrderService.java
+++ order-service/src/main/java/com/microservices/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.microservices.order.service;
 
+import com.microservices.order.client.InventoryClient;
 import com.microservices.order.dto.OrderRequest;
 import lombok.RequiredArgsConstructor;
 import com.microservices.order.model.Order;
@@ -13,17 +14,24 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
     public void placeOrder(OrderRequest orderRequest) {
-        // map orderRequest to Order object
-        Order order= new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setOrderNumber(orderRequest.orderNumber());
-        order.setSkuCode(orderRequest.skuCode());
-        order.setPrice(orderRequest.price());
-        order.setQuantity(orderRequest.quantity());
 
-        //save Order using order repository
-        orderRepository.save(order);
+        boolean isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+        if (isProductInStock) {
+            // map orderRequest to Order object
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setOrderNumber(orderRequest.orderNumber());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setPrice(orderRequest.price());
+            order.setQuantity(orderRequest.quantity());
+
+            //save Order using order repository
+            orderRepository.save(order);
+        } else {
+            throw new RuntimeException("Product with skuCode: " + orderRequest.skuCode() + " is not present");
+        }
     }
 }
