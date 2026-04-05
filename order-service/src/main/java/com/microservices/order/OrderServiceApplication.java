b7a34f7e76e480611f3fe4cca4bd242411c38936
Yashkumar Gautam <yk.gautam97@gmail.com>
2026-04-05 12:54:10 +0530

    #order-service - Used RestClient and commented OpenFeign config code

--- order-service/src/main/java/com/microservices/order/OrderServiceApplication.java
+++ order-service/src/main/java/com/microservices/order/OrderServiceApplication.java
@@ -2,10 +2,9 @@ package com.microservices.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
+//@EnableFeignClients
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
--- order-service/src/main/java/com/microservices/order/client/InventoryClient.java
+++ order-service/src/main/java/com/microservices/order/client/InventoryClient.java
@@ -1,14 +1,14 @@
 package com.microservices.order.client;
 
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.service.annotation.GetExchange;
 
-@FeignClient(value = "inventory", url = "http://localhost:8082")
+//@FeignClient(value = "inventory", url = "http://localhost:8082")
 public interface InventoryClient {
 
-    @RequestMapping(method = RequestMethod.GET, value = "/api/inventory/is-in-stock")
+//    @RequestMapping(method = RequestMethod.GET, value = "/api/inventory/is-in-stock")
+
+    @GetExchange("/api/inventory/is-in-stock")
     boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
 
 }
